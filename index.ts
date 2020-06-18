b3c59d5095b1d437cd70b446b37a27de6bbb285b
linuxonline4444 <linuxonline4444@gmail.com>
2020-06-19 01:34:49 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,6 @@
 // Write TypeScript code!
 //const appDiv: HTMLElement = document.getElementById('app');
 //appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let num1: number;
+num1=10;
+console.log(num1);
